a5d2cab313337a9575298df4339760759030b89b
Yossava Adhi <yossava.adhi@insurediy.com>
2024-01-24 14:29:31 +0700

    blog page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
+import Footer from "@/components/Footer";
+import NavbarAlt from "@/components/NavbarAlt";
 import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 import "../app/globals.css";
 
 export const metadata: Metadata = {
   title: "Able Group",
   description: "Able Group",
 };
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
 export default function App({
   Component,
   pageProps,
@@ -12,5 +21,11 @@ export default function App({
   Component: React.FC;
   pageProps: any;
 }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NavbarAlt />
+      <Component className={poppins.className} {...pageProps} />
+      <Footer />
+    </>
+  );
 }
